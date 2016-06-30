8e38c30a57d826971c08b297df967040dfdffe97
Paul-Louis NECH <pln@algolia.com>
2016-06-30 14:12:18 +0200

    Tools: Improve publish script

--- publish_android.mac.sh
+++ publish_android.mac.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-
-keytool -genkey -v -keystore release.keystore -alias PLNech -keyalg RSA -keysize 2048 -validity 10000
+cd app && ionic build android --release && cd ..
+# Uncomment to create a new keystore
+#keytool -genkey -v -keystore release.keystore -alias PLNech -keyalg RSA -keysize 2048 -validity 10000
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore release.keystore /Users/pln/Work/Perso/EuropApp/app/platforms/android/build/outputs/apk/android-release-unsigned.apk PLNech
+rm -f EuropApp.apk.old; mv EuropApp.apk EurpApp.apk.old
 zipalign -v 4 app/platforms/android/build/outputs/apk/android-release-unsigned.apk EuropApp.apk
--- publish_android.sh
+++ publish_android.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-
-/cygdrive/c/Program\ Files/Java/jre1.8.0_73/bin/keytool.exe -genkey -v -keystore release.keystore -alias PLNech -keyalg RSA -keysize 2048 -validity 10000
+cd app && ionic build android --release && cd ..
+# Uncomment to create a new keystore
+#/cygdrive/c/Program\ Files/Java/jre1.8.0_73/bin/keytool.exe -genkey -v -keystore release.keystore -alias PLNech -keyalg RSA -keysize 2048 -validity 10000
 /cygdrive/c/Program\ Files/Java/jdk1.8.0_05/bin/jarsigner.exe -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore release.keystore C:/Users/PLN/Work/iGEM/EuropApp/app/platforms/android/build/outputs/apk/android-release-unsigned.apk PLNech
+rm -f EuropApp.apk.old; mv EuropApp.apk EurpApp.apk.old
 /cygdrive/c/Android\ SDK/sdk/build-tools/23.0.2/zipalign.exe -v 4 app/platforms/android/build/outputs/apk/android-release-unsigned.apk ../EuropApp.apk
