c509005cb9019cd839b6c4ff9d7b3a28bec895cd
Paul-Louis NECH <pln@algolia.com>
2016-06-30 14:32:33 +0200

    Publish: unified release scripts

--- publish_android.mac.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-cd app && ionic build android --release && cd ..
-# Uncomment to create a new keystore
-#keytool -genkey -v -keystore release.keystore -alias PLNech -keyalg RSA -keysize 2048 -validity 10000
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore release.keystore /Users/pln/Work/Perso/EuropApp/app/platforms/android/build/outputs/apk/android-release-unsigned.apk PLNech
-rm -f EuropApp.apk.old; mv EuropApp.apk EurpApp.apk.old
-zipalign -v 4 app/platforms/android/build/outputs/apk/android-release-unsigned.apk EuropApp.apk
--- publish_android.sh
+++ publish_android.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
+if [ "$(uname)" == "Darwin" ]; then
+    KEYTOOL=keytool
+    JARSIGNER=jarsigner
+    ZIPALIGN=zipalign
+else
+    KEYTOOL=/cygdrive/c/Program\ Files/Java/jre1.8.0_73/bin/keytool.exe
+    JARSIGNER=/cygdrive/c/Program\ Files/Java/jdk1.8.0_05/bin/jarsigner.exe
+    ZIPALIGN=/cygdrive/c/Android\ SDK/sdk/build-tools/23.0.2/zipalign.exe
+fi
+
 cd app && ionic build android --release && cd ..
 # Uncomment to create a new keystore
-#/cygdrive/c/Program\ Files/Java/jre1.8.0_73/bin/keytool.exe -genkey -v -keystore release.keystore -alias PLNech -keyalg RSA -keysize 2048 -validity 10000
-/cygdrive/c/Program\ Files/Java/jdk1.8.0_05/bin/jarsigner.exe -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore release.keystore C:/Users/PLN/Work/iGEM/EuropApp/app/platforms/android/build/outputs/apk/android-release-unsigned.apk PLNech
-rm -f EuropApp.apk.old; mv EuropApp.apk EurpApp.apk.old
-/cygdrive/c/Android\ SDK/sdk/build-tools/23.0.2/zipalign.exe -v 4 app/platforms/android/build/outputs/apk/android-release-unsigned.apk ../EuropApp.apk
+#$KEYTOOL -genkey -v -keystore release.keystore -alias PLNech -keyalg RSA -keysize 2048 -validity 10000
+$JARSIGNER -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore release.keystore C:/Users/PLN/Work/iGEM/EuropApp/app/platforms/android/build/outputs/apk/android-release-unsigned.apk PLNech
+rm -f EuropApp.apk.old; mv EuropApp.apk EuropApp.old.apk
+$ZIPALIGN -v 4 app/platforms/android/build/outputs/apk/android-release-unsigned.apk EuropApp.apk
